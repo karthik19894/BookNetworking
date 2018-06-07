155a02711fa80fb02eb3c23916a8546624fb5862
Karthik <karthik19894@gmail.com>
2018-06-07 21:19:17 +0530

    Added the Functionality for Adding a New Book to Global Catalogue

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ router=express.Router(),
 Book=require('../models/book');
 
  
-
+//Route for displaying All the available Books
 router.get('/',function(req,res){
     Book.find({},function(err,books){
     if(err){
@@ -16,6 +16,30 @@ router.get('/',function(req,res){
     
 })
 });
+
+//Route For Adding New Books to the Global Catalogue
+router.get('/new',(req,res)=>{
+    res.render('books/newbook');
+});
+
+router.post('/',(req,res)=>{
+    //Getting the book details from the form through body parser
+    const new_book=req.body.book;
+
+    Book.create(new_book,(err,created_book)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("New Book Added Successfully");
+            res.redirect('/books');
+        }
+    });
+
+
+});
+
+
 
 
 module.exports = router;
