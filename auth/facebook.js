0de857dbb8fccc62bf7ca94a27e4cd48dbe9f848
Karthik <karthik19894@gmail.com>
2018-06-10 21:52:03 +0530

    restricted client secrets using env variables

--- auth/facebook.js
+++ auth/facebook.js
@@ -3,9 +3,9 @@ var passport = require('passport')
 var User = require('../models/user');
 
 passport.use(new FacebookStrategy({
-    clientID: '249606868923189',
-    clientSecret: '793231727e62eca4b8d9c8e5ba407ef6',
-    callbackURL: "http://localhost:8088/auth/facebook/callback"
+    clientID: process.env.FBCLIENTID,
+    clientSecret: process.env.FBCLIENTSECRET,
+    callbackURL: "http://localhost:"+process.env.PORT+"/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({username: profile.displayName}, {username: profile.displayName}, function(err, user) {
--- auth/google.js
+++ auth/google.js
@@ -3,8 +3,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/user');
 
 passport.use(new GoogleStrategy({
-    clientID: "952021953094-0r931qboagbjn651onik34gbked2i52r.apps.googleusercontent.com",
-    clientSecret: "ooOKT-ajvvMjbIflhOkdwROj",
+    clientID: process.env.GCLIENTID,
+    clientSecret: process.env.GCLIENTSECRET,
     callbackURL: "http://localhost:"+process.env.PORT+"/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
